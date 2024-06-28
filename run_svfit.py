4f3b7cbfec0d1a3f55e66fdf87f34cf5233c41ec
Valentina Camagni <v.camagni1@campus.unimib.it>
2024-06-28 17:23:51 +0200

    run on the branches of the input root file

--- run_svfit.py
+++ run_svfit.py
@@ -33,22 +33,22 @@ svfit_string = '''
 auto svfit = StandaloneSVfit();
 
 int verbosity    = 0;
-int pairType     = 0;
-int DM1          = -1;
-int DM2          = 1;
-double tau1_pt   = 42.122390;
-double tau1_eta  = -0.149487;
-double tau1_phi  = 0.1000680;
-double tau1_mass = 0.10874185;
-double tau2_pt   = 111.85233;
-double tau2_eta  = -0.499875;
-double tau2_phi  = 1.218472;
-double tau2_mass = 1.1941469;
-double met_pt    = 46.620235;
-double met_phi   = 5.5289621;
-double met_covXX = 834.84625;
-double met_covXY = 234.44636;
-double met_covYY = 915.31604;
+int pairType     = 2;
+//int DM1          = -1;
+//int DM2          = 1;
+//double tau1_pt   = 42.122390;
+//double tau1_eta  = -0.149487;
+//double tau1_phi  = 0.1000680;
+//double tau1_mass = 0.10874185;
+//double tau2_pt   = 111.85233;
+//double tau2_eta  = -0.499875;
+//double tau2_phi  = 1.218472;
+//double tau2_mass = 1.1941469;
+//double met_pt    = 46.620235;
+//double met_phi   = 5.5289621;
+//double met_covXX = 834.84625;
+//double met_covXY = 234.44636;
+//double met_covYY = 915.31604;
 
 double massHypo = 125.; // set to -1 if you don't want to pass a mass hypothesis
 
@@ -56,7 +56,7 @@ auto result = svfit.FitAndGetResultWithInputs(
     verbosity, pairType, DM1, DM2,
     tau1_pt, tau1_eta, tau1_phi, tau1_mass,
     tau2_pt, tau2_eta, tau2_phi, tau2_mass,
-    met_pt, met_phi, met_covXX, met_covXY, met_covYY,
+    met_pT, met_phi, met_covXX, met_covXY, met_covYY,
     massHypo);
 return result;
 '''
